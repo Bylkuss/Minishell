89f56f90562a37e480241920f90b194aa06a640f
GHebert42 <hebertgermain.qc@gmail.com>
2022-11-14 09:53:27 -0500

    oups_minishell_h was left behind

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,13 +2,19 @@
 # define MINISHELL_H
 
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include "../includes/get_next_line.h"
 # include "../libft/incs/libft.h"
+# include "../includes/get_next_line.h"
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 /* fd Refs*/
 # define READ_END 0
@@ -43,33 +49,30 @@ enum TokenType{
 	TOKEN = -1,
 };
 
-typedef struct s_dot t_dot;
-typedef struct s_token t_token;
-typedef struct s_lexer t_lexer; 
-typedef struct s_mini t_mini;
+/* Token's end type */
+enum EndType{
+	ERR_END   = 0,
+	OUTF1_END = 1,
+	OUTF2_END = 2,
+	INF1_END  = 3,
+	INF2_END  = 4,
+	PIPE_END  = 5,
+	DEAD_END  = 6
+};
 
-struct s_dot
+typedef struct s_dot t_dot;
+typedef struct s_mini t_mini;
+typedef struct s_token t_token;
+// typedef struct s_lexer t_lexer; 
+
+struct s_dot		/*  ENVP BUILDER */
 {
-	char *cmds;
+	t_list *cmds;
 	char **envp;
 	pid_t	pid;
 };
 
-struct s_token
-{
-	char *data;
-	int type;
-	t_token *next;
-};
-
-
-struct s_lexer
-{
-	t_token *listok;
-	int  ntok;
-};
-
-struct s_mini
+struct s_mini 	 	/*	MATRIX COMMAND TABLE */
 {
 	char **full_cmd;
 	char *full_path;
@@ -77,20 +80,43 @@ struct s_mini
 	int outfile;
 };
 
+struct s_token		/*	 THREE-PART NODE-FORM TOKEN		*/
+{
+	char *cmd;
+	char *arg;
+	int	endtype;
+	t_token *next;
+};
+
 //signal
 void    	handle_sigint(int sig);
-char		*mini_getenv(char *var, char **envp, int n);
-char		**mini_setenv(char *var, char *value, char **envp, int n);
+char	*ms_getenv(char *var, char **envp, int n);
+char	**ms_setenv(char *var, char *value, char **envp, int n);
 // //prompt 
- char   		*mini_getprompt(t_dot p);
-//  static t_mini	init_vars(t_dot prompt, char *str, char **av);
-//  static t_dot	init_prompt(char **av, char **envp); 
-// //parse
-t_dot 	parse_args(char **args, t_dot p);
-t_dot  	check_args(char *out, t_dot p);
-// //subsplit
-char 			**ft_cmdtrim(const char *s, char *set);
+char   	*getprompt(t_dot p);
+//parse
+void	*check_args(char *out, t_dot *p);
+//subsplit
+char 	**ft_cmdtrim(const char *s, char *set);
+//divide
+char	**ft_cmd_div(char const *s, char *set);
 //update
-// void    exec_custom(char **out, char *full, char *args, char **envp) ;
+void    exec_custom(char ***out, char *full, char *args, char **envp) ;
+//expand
+char	*expand_vars(char *str, int i, int quotes[2], t_dot *p);
+char	*expand_path(char *str, int i, int quotes[2], char *var);
+//nodes
+t_list	*fill_nodes(char **args, int i);
+//redir
+t_mini *get_outfile1(t_mini *token, char **args, int *i);
+t_mini *get_outfile2(t_mini *token, char **args, int *i);
+t_mini *get_infile1(t_mini *token, char **args, int *i);
+// t_mini *get_infile2(t_mini *token, char **args, int *i);
+int	get_fd(int oldfd, char *path, int flags[2]);
+//trimm_all
+char        *ft_strtrim_all(const char *s, int squote, int dquote);
+//display
+void display_form(t_mini *mx);
+
 
 #endif
