b68d1d9de0625ce1a1df5fe482102d504bee1225
Lounes Adjou <lmatoub91@gmail.com>
2022-11-29 16:36:27 -0500

    unset much improved

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,12 +6,19 @@
 /*   By: bylkus <bylkus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:12:07 by bylkus            #+#    #+#             */
-/*   Updated: 2022/11/29 15:18:12 by bylkus           ###   ########.fr       */
+/*   Updated: 2022/11/29 16:31:53 by bylkus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+int ft_strcmp_u(char *s1, char *s2, char until)
+{
+    int i = 0;
+    while(s1[i] && s2[i] && s1[i] != until)
+        i++;
+    return s1[i] - s2[i];
+}
 
 void    env(char **envp)
 {
@@ -42,7 +49,7 @@ void    unset(char **cmd, char **envp)
     }
     while(envp[i])
     {
-        if(ft_strnstr(envp[i], cmd[1], ft_strlen(cmd[1])))
+        if(!ft_strcmp_u(envp[i], cmd[1], '='))
         {
             printf("VAR to unset: [%d] %s\n\n", i, envp[i]);
             if(!envp[i + 1])
