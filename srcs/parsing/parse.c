6e5988447aeb1b1cc372e4a3679b639c083ef5db
GHebert42 <hebertgermain.qc@gmail.com>
2022-12-10 00:04:11 -0500

    mega_mind

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -14,11 +14,69 @@
 
 extern int g_status;
 
-// t_token	*token_nodes(t_table *tab)	/* call by parse_  <<(token_ized)	*/
-// {
+static int	token_etype(t_table *tab) //, char **a[2])//, int *i) // endtype (int)
+{
+    t_token *token;
+    char **cmd;
+    int id;
 
-    
-// }
+    id = 0;
+    cmd = tab->node; //[id][node[;]
+    token = tab->token;
+		//
+			//	get to *node[0]	== token->cmds
+			//	get *node[len-1] == 
+			//	get *node[len]	== token->endtype
+				// DEAD_END  = 0,
+				// PIPE_END  = 1,
+				// OUTF1_END = 2,
+				// OUTF2_END = 3,
+				// INF1_END  = 4,
+				// INF2_END  = 5,
+				// ERR_END   = 6
+
+		// while (id <= tab->tk_num)
+		// {
+            printf("\nendtype\n");
+        if (token->cmd && (token->id < token->tk_len) && (token->id < token->tk_len))
+        {
+            // if (tab->cmds == ">" && mx[id][cmd + 1] && mx[id][cmd + 1][nod] == '>')
+            // 	token = get_outfile2(token, mx[id]);//nod
+            if (*cmd[id] == '>' && cmd  && *cmd[id + 1] == '>')
+                tab->token->endtype = 3;	
+                // if (mx[id][cmd][nod] == '>' && mx[id][cmd + 1] && mx[id][cmd + 1][nod] == '>')
+                // 	token = get_outfile2(token, mx[id]);//nod
+
+
+            else if (*cmd[id] == '>')
+                tab->token->endtype = 2;
+                        // token = get_outfile1(token, mx[id]);//nod
+                        // else if (a[0][*i][0] == '<' && a[0][*i + 1] && 
+                        // 	a[0][*i + 1][0] == '<')
+                        // 	m = get_infile2(m, a[1], i);*/
+                        // else if (mx[id][cmd][nod] == '<')
+                        // 	token = get_infile1(token, mx[id]);	//nod
+
+            else if (*cmd[id] != '|')
+                tab->token->endtype = 1;
+            else if (*cmd[id] != '\0')
+                tab->token->endtype = 0;
+                
+        }
+						// 	m->full_cmd = ft_mx_ext(m->full_cmd, a[1][*i]);
+						// else
+						// {
+							//mini_perror(PIPENDERR, NULL, 2);
+							// *i = -2;
+						// }
+						// return (token);
+		// }	
+		// mini_perror(PIPENDERR, NULL, 2);
+		// *i = -2;
+        return (tab->token->endtype);
+}
+// set == endtype char_split	i[x] == start_pos/sub_end/end_pos
+
 
 /*       char **args = tab->cmds  :  token chunk    */
 static t_table *split_all(char **node, t_table *tab)  
@@ -75,39 +133,12 @@ static t_table *split_all(char **node, t_table *tab)
         }
         
  //  ls  -lta | wc -l >> out.txt 
+
  //  ls  -lta| head -2| wc -l >> out.txt 
 
-            // printf("DEBUG: focus->id[%d] :: tkn->len {%d} ::\n", focus_id, ft_mx_len(tab->token->cmd));
-    // }
-        // if (tab->tk_num > 0)
-        //     {
-        //         tab->token->cmd = ft_mx_ext(tab->token->cmd, node[i]);
-        //         ++id;
-        //         focus_id = tab->token->id;               
-        //         if (ft_strchar_i(node[i], set))        
-        //             tab->token->cmd = ft_mx_ext(tab->token->cmd, "\0");
-        //         tab->tk_num--;
-        //         if (tab->tk_num == 0) 
-        //         {
-        //             tab->cmds[tab->token->id] = ft_mx_dup(tab->token->cmd);
-        //             ft_mx_free(&tab->token->cmd);
-        //             tab->token->id++;           // can be place before dup
-        //         } 
-        //     }
-        // printf("DEBUG: focus->id[%d] :: tkn->len {%d} ::\n", tab->token->id, ft_mx_len(tab->token->cmd));
-    // tab->token = token_nodes(tab);
+         // tab->token = token_nodes(tab);
         // printf("tk_id[%d] ==> ...%s... \n", id, tab->token->cmd[i]);
-            // printf("DEBUG: .. .. FOCUS_id[%d]\n",focus_id);
-            // tab->token = token_nodes(tab);
-            // tab->cmds[id] = ft_mx_rpl(tab->cmds, tab->token->cmd, ft_mx_len(tab->token->cmd));
-            // mx_display_tab(tab->token->cmd);
-            // tab->token->tk_len += ft_mx_len(tab->node);
-                    // mx_rpl (arg , node)
-                    // ft_mx_rpl(&args, nodes, i);
-                    // //
-                    // // printf("mx_len == %d\n", i);
-                    // // free node
-                    // ft_mx_free(&nodes);
+
 
         // printf("DEBUG: split_all :: tk_id = %d\n", id);
         // printf("DEBUG: split all :: t->cmd = %s\n", tab->token->cmd[i]);
@@ -127,20 +158,15 @@ static t_token  *parse_args(t_table *tab)
     i = 0;
     tk_id = 0;
     type_id = 0;
-                //     tab >> tab->node  ::  substr( tab->cmds >> endtype ) 
-                // if (tab->node)
-            // printf("DEBUG: parse... tab->cmds >> \n");
-                //create cmds ... token ... malloc both!
             
     tab = split_all(tab->node, tab); 
-            // tab_len =  ft_mx_len(tab->node);
     printf("DEBUG: parse >>token_num = [%d] \n",tab->tk_num);
 
-    // //
-    //
-    // type_id = endtype(tab);
-    //
-    // //
+        // //
+        token->endtype = token_etype(tab);
+        // type_id = endtype(tab);
+        //
+        // //
 
             // tab->cmds[tk_id] = ft_mx_rpl(tab->cmds, tab->node, tab_len);
             // tab_len = ft_mx_len(tab->cmds[i]);
