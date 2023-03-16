2efefce041ed9a15971d4d511ada2c7e68d409ae
GHebert42 <hebertgermain.qc@gmail.com>
2023-03-16 10:10:47 -0400

    mini

--- srcs/operators/redir.c
+++ srcs/operators/redir.c
@@ -6,14 +6,13 @@
 /*   By: gehebert <gehebert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 21:28:46 by gehebert          #+#    #+#             */
-/*   Updated: 2023/03/12 15:40:04 by gehebert         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:03:29 by gehebert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../includes/minishell.h"
 
-extern int g_status;
+extern int	g_status;
 
 int	get_fd(int oldfd, char *path, int flags[2])
 {
@@ -28,10 +27,10 @@ int	get_fd(int oldfd, char *path, int flags[2])
 	else if (!flags[0] && access(path, R_OK) == -1)
 		chk_error(NPERM, path, 126);
 	else if (flags[0] && access(path, W_OK) == -1 && access(path, F_OK) == 0)
-		chk_error(NPERM, path, 126); 
+		chk_error(NPERM, path, 126);
 	if (flags[0] && flags[1])
 		fd = open(path, O_CREAT | O_WRONLY | O_APPEND, 0666);
-	else if (flags[0] && !flags[1]) //(flags[0] && !flags[1]) 
+	else if (flags[0] && !flags[1]) //(flags[0] && !flags[1])
 		fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 	else if (!flags[0] && oldfd != -1) //(!flags[0] && oldfd != -1)
 		fd = open(path, O_RDONLY);
@@ -44,9 +43,9 @@ t_node	*get_outfile1(t_node *t, char **a, int *i)
 {
 	char	*nl;
 	int		flags[2];
+
 	// int 	id;
 	// id = 0;
-
 	flags[0] = 1;
 	flags[1] = 0;
 	nl = "minishell: syntax error near unexpected node '>'";
@@ -119,23 +118,23 @@ t_node	*get_infile1(t_node *t, char **a, int *i)
 	return (t);
 }
 
-t_node	*get_infile2(t_node *t,  char **a, int *i)
+t_node	*get_infile2(t_node *t, char **a, int *i)
 {
-	char	*aux[2];
-	char	*nl;
-	char	*str[2];
+	char *aux[2];
+	char *nl;
+	char *str[2];
 	str[0] = NULL;
 	str[1] = NULL;
 	aux[0] = NULL;
 	aux[1] = "minishell: warning: here-document delimited by end-of-file";
 	nl = "minishell: syntax error near unexpected node `<<'";
 	(*i)++;
-	if (a[(*i)])
-		
+	// if (a[(*i)])
+
 	if (a[++(*i)])
 	{
 		aux[0] = a[*i];
-		t->infile = get_here_doc(str, aux);  /*later */
+		t->infile = get_here_doc(str, aux); /*later */
 	}
 	if (!a[*i] || t->infile == -1)
 	{
